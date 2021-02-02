4a8939607541e15ffca285149045295d568a5d11
Jason Robock <jrobock@ualberta.ca>
2021-02-01 18:40:08 -0700

    Stuffs

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 #this gets consulted only if the url starts with baseurl+/polls/
 urlpatterns = [
-  path('', views.index, name='index')
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
